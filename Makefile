ad78b77f4b6703e3950bfab4458cde7a3442e09d
rasel <rasel@appscode.com>
2024-08-17 00:13:31 +0600

    Add Dockerfile
    Signed-off-by: rasel <rasel@appscode.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,17 @@
 PORT := ":8080"
 
+build:binary
+	@docker build
 binary:
-	@go build ./cmd/account-server/*
+	@go build ./cmd/app/*
 
 run: binary
-	@./account-server --port $(PORT)
+	@./app-server --port $(PORT)
+
+run-docker:
+	@docker build --tag superm4n/api-server .
+	@docker run -dp 8080:8080 --name my-api-server superm4n/api-server
+	@docker container rm -f my-api-server || true
+	@docker start my-api-server -a
+#you can exec into the container using
+#	@docker exec -it my-account-server sh
--- pkg/cmds/root.go
+++ pkg/cmds/root.go
@@ -4,6 +4,13 @@ import (
 	"github.com/Superm4n97/account-server/server"
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
+	"os"
+)
+
+const (
+	envPort   = "PORT"
+	envDB     = "DB_URI"
+	envDbName = "DB_NAME"
 )
 
 func NewRootCmd() *cobra.Command {
@@ -16,12 +23,25 @@ func NewRootCmd() *cobra.Command {
 		Use:   "account-server",
 		Short: "Launch Account Server",
 		Run: func(cmd *cobra.Command, args []string) {
+			if v, exists := os.LookupEnv(envPort); exists {
+				port = v
+			}
+			if v, exists := os.LookupEnv(envDB); exists {
+				dbUri = v
+			}
+			if v, exists := os.LookupEnv(envDbName); exists {
+				dbName = v
+			}
+
 			s := server.Server{
 				Router:       server.NewRouter(),
 				Port:         port,
 				DatabaseURI:  dbUri,
 				DatabaseName: dbName,
 			}
+			klog.Infof("using server port: %s\n", port)
+			klog.Infof("using database uri: %s\n", dbUri)
+			klog.Infof("api server started...")
 			if err := s.Start(); err != nil {
 				klog.Error(err.Error())
 			}
